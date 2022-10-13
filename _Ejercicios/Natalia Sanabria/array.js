0f384a2df397af3a238f7a1d66f588cf18025d6a
unknown <natalia.sanabria@arbusta.net>
2022-10-13 12:04:23 -0300

    Agrego ejercicios

---
+++ _Ejercicios/Natalia Sanabria/array.js
@@ -0,0 +1,4 @@
+//Arrays- Puedo pasarle cualquier tipo de datos,num,string,booleano,incluso hasta otro array- A esto se le llama un Array bidimensional
+let colores= ["rojo","verde","azul","amarillo","blanco",["negro"]]
+document.write (colores)
+
