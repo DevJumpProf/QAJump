0c4708ca3748d6247f570e31038d24a1a4dc9f5e
Arbusta <gabriel.munoz@arbusta.net>
2022-10-14 10:02:50 -0300

    ejercicios

--- _Ejercicios/Yael Lara Ramirez - copia/1. Ejercicios de Variables y concatenación/BilletaraSantaFe.psc
+++
@@ -1,8 +0,0 @@
-Algoritmo BilletaraSantaFe
-	Definir num1 Como Entero
-	Definir resultado Como Real
-	Escribir 'Inserta el monto del producto'
-	Leer num1
-	resultado <- num1-(num1*30/100)
-	Escribir "El monto del producto es ", resultado
-FinAlgoritmo
--- _Ejercicios/Yael Lara Ramirez - copia/array.js
+++
@@ -1,7 +0,0 @@
-
-// Array: caja con varios datos. Puede mezclar datos y dentro de un mismo array poner strings, números, booleanos e incluso otro array dentro //
-let musica = ["piano", "trompeta", "bajo", "guitarra"]
-let heroes = ["Batman", "Superman", "Mujer Maravilla",["Wolverine", "Iron Man", "Spiderman"]]
-document.write (heroes[3])
-
-//pseint se numeran los elementos de la lista del 1 en adelante, en JS se numeran comenzando con el 0 //
