55013d6d2131f13935442e73176acb051bcb5d5a
gustavo.almendras@arbusta.net <gustavo.almendras@arbusta.net>
2022-10-21 14:07:46 -0300

    Agrego ejercicios practicos

--- _Ejercicios/Gustavo Almendras/array.js
+++ _Ejercicios/Gustavo Almendras/array.js
@@ -6,4 +6,15 @@ let musica = [true, 32, "Bongo", "Campana", "Timbales", "Piano",["cables","mic"]
 
 let heroes = ["Batman","Superman","Mujer Maravilla","Linterna Verde", "She Hulk"]
 
+
+//Metodos
+
+heroes.pop() //Elimina el ultimo dato del array
+
+heroes.push("Iron Man") //Agrega el dato al final
+
+heroes.shift() //Elimina el primer elemento del array
+
+heroes.unshift("Ant-man") //Agrega elementos al inicio.
+
 document.write (heroes)
