2345a48f3bd36554dd61e0d100fadd789b3b0b9a
unknown <natalia.sanabria@arbusta.net>
2022-10-21 14:59:07 -0300

    Agrego ejercicios

---
+++ _Ejercicios/Natalia Sanabria/funciones.js
@@ -0,0 +1,25 @@
+/*num1= 23
+num2= 27
+resultado= num1+num2
+document.write(resultado)*/
+
+/*Funciones 
+function suma (num1,num2){
+document.write("El resultado de tu suma es: " + (num1 + num2))
+}
+
+suma(10,50)
+suma(100,550)
+suma(100,850)*/
+
+function mayor (nombre,edad){
+    if(edad>=18){
+        document.write(nombre + ",podes pasar" + "<br>")
+
+    }else{
+        document.write(nombre + ",no podes pasar tenes " + edad + " años")
+
+    }
+}
+mayor("Bruno", 18)
+mayor ("Alicia", 15)
