75c11d96efe164d08dd7f4b0235d2332c9371361
Camila Scutari <camila.scutari@arbusta.net>
2022-10-23 15:34:55 -0300

    Ejercicios de lógica cargados

--- _Ejercicios/Camila Scutari/array.js
+++ _Ejercicios/Camila Scutari/array.js
@@ -7,5 +7,14 @@ let musica = [true, 32, "Bongo", "Campana", "Timbales", "Piano",["cables","mic"]
 let heroes = ["Batman","Superman","Mujer Maravilla","Linterna Verde", "She Hulk"]
 // Los arrays tienen indices. Siempre comienzan desde cero.
 
-document.write (musica[6][1])
 
+//metodos
+heroes.pop() // elimina el ultimo elemento del array
+
+heroes.push("Iron Man") // agrega un dato al final
+
+heroes.shift() // elimina el primero elementodel array
+
+heroes.unshift("Ant Man") // agrega el primer elemento del array
+
+document.write (heroes)
