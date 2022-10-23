a5539b2d2fa5106b4eaec7f933b3d24e19a2f1a0
Arbusta <kpaiva@arbusta.net>
2022-10-23 00:26:00 -0300

    Subi ejercicios

--- _Ejercicios/Karina Paiva/array.js
+++ _Ejercicios/Karina Paiva/array.js
@@ -6,4 +6,15 @@ let heroes = ["Batman","Spiderman","Mujer Maravilla","Linterna verde","she Hulk"
 
 document.write("Hola Arbusta")
 
+
 document.write(heroes[1])
+
+heroes.pop() //elimina el ultimo elemento de Array
+
+heroes.push("Iron Man") //agrega un dato al final
+
+heroes.shift() //elimina el primer elemento del array
+
+heroes.unshift("Ant Man")  // agrega el primer elemento
+
+
