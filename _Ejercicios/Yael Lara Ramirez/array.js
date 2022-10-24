589feeadff055e5040d512f0589e85c848654114
unknown <yael.ramirez@arbusta.net>
2022-10-24 08:22:10 -0300

    actualización de ejercicios

--- _Ejercicios/Yael Lara Ramirez/array.js
+++ _Ejercicios/Yael Lara Ramirez/array.js
@@ -5,3 +5,16 @@ let heroes = ["Batman", "Superman", "Mujer Maravilla",["Wolverine", "Iron Man", 
 document.write (heroes[3])
 
 //pseint se numeran los elementos de la lista del 1 en adelante, en JS se numeran comenzando con el 0 //
+
+//metodos
+
+heroes.pop () //elimina el último elemento del array//
+document.write (heroes)
+
+heroes.push ("Iron Man") //Agrega un dato al final//
+document.write (heroes)
+
+heroes.shift () //agrega el primer elemento del array//
+
+heroes.unshift ("Ant Man") //
+
